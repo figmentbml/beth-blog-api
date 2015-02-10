4f59192c1f9bab6c5b149ac21a298ca5d28c5372
figmentbml <beth.jaswa@gmail.com>
2015-02-10 13:13:24 -0700

    lookin good

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
 
   def index
-    render json: Post.all, except: [:comments, :users]
+    searched = params[:find]
+    if searched
+      @filtered = Post.where("title LIKE ? OR body LIKE ?", "%#{searched}%", "%#{searched}%")
+
+      render json: @filtered, except: [:comments, :users]
+    else
+      render json: Post.all, except: [:comments, :users]
+    end
   end
 
   def show
     render json: Post.find(params[:id]), except: [:posts]
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :user_id, :comment_id)
   end
 end
