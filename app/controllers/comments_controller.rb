b8ed200f039f77da622b63b8a845aae2c86def24
figmentbml <beth.jaswa@gmail.com>
2015-02-18 15:02:04 -0700

    finished crud on comments and users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
 
   def index
     render json: Comment.all
@@ -7,4 +8,32 @@ class CommentsController < ApplicationController
   def show
     render json: Comment.find(params[:id])
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    render json: @comment
+  end
+
+  def update
+    @comment.update(comment_params)
+    @comment.save
+    render json: @comment
+  end
+
+  def destroy
+    @comment.destroy
+    render json: {}
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :post_id)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
 
   def index
     render json: User.all, except: [:comments, :posts]
@@ -6,5 +7,32 @@ class UsersController < ApplicationController
 
   def show
     render json: User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    render json: @user
+  end
+
+  def update
+    @user.update(user_params)
+    @user.save
+    render json: @user
+  end
+
+  def destroy
+    @user.destroy
+    render json: {}
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :post_id, :comment_id)
   end
 end
